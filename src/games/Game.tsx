620dac2a22fa908e27bd71646bce1b30016acace
Max <max.kononerov@gmail.com>
2025-06-06 19:47:19 +0400

    The game base is working

--- src/games/Game.tsx
+++ src/games/Game.tsx
@@ -2,45 +2,95 @@ import React, {useState} from 'react'
 
 import './game.css'
 
-type Board = string
+interface GameState {
+    board: Cell[];
+    currentPlayer: 'X' | "O";
+    winner: Cell | null | 'Ничья',
+}
+
+type WinCell = Cell | null | 'Ничья';
+
+type Cell = '' | 'X' | 'O';
+
+const Combination: readonly number[][] = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+];
 
 const Game: React.FC = () => {
-    const [board, setBoard] = useState<Board[]>(['','','','','','','','','']);
+    const [game, setGame] = useState<GameState>({
+        board: Array(9).fill(''),
+        currentPlayer: 'X',
+        winner: null,
+    });
 
-    const [currentPlayer, setCurrentPlayer] = useState('X');
-
-    const handleClick = (index) => {
-      
-        if(board[index] === '') {
-            setBoard(board.map(item => {
-                if(item[index] === index) {
-                    const newBoard = [...board];
-                    newBoard[index] = currentPlayer;
-                    setBoard(newBoard)
-                }
-                    return item;
-            }))
-
-            if(currentPlayer === 'X') {
-                setCurrentPlayer('O')
-            } else if(currentPlayer === 'O') {
-                setCurrentPlayer('X')
+    const checkWinner = (board: Cell[]): WinCell  => {
+        for(const combo of Combination) {
+            const [a, b, c] = combo;
+            if(board[a] && board[a] === board[b] && board[a] === board[c]) {
+                return board[a];
             }
-        } 
+        }
+        return null;
     }
 
+    const handleClick = (index: number): void => {
+        if(game.winner !== null) {
+            return 
+        }
+
+        if(game.board[index] === '') {  
+            const newBoard = [...game.board];
+            newBoard[index] = game.currentPlayer;
+            let winCell = checkWinner(newBoard);
+            if (!winCell && newBoard.every(cell => cell !== '')) {
+                winCell = 'Ничья';
+            }
+            setGame({
+                ...game,
+                board: newBoard,
+                winner: winCell,
+                currentPlayer: game.currentPlayer === 'X' ? 'O' : 'X'
+            });
+        }
+    } 
+
+    const restartGame = () => {
+        setGame({
+            board: Array(9).fill(''),
+            currentPlayer: 'X',
+            winner: null
+        });
+    };
+   
     return (
-        <div className="game-wrap">
-            {board.map((cell, index) => (
-                <button 
-                    className="game-input" 
-                    key={index}
-                    onClick={() => handleClick(index)}
-                >
-                        {cell}
+
+        <div>
+            {game.winner === 'Ничья' ? ( <div>Ничья</div> ) : game.winner ? (
+                <div>Победил - {game.winner}</div>
+            ): null}
+        
+            <div className="game-wrap">
+                {game.board.map((cell, index) => (
+                    <button 
+                        className="game-input" 
+                        key={index}
+                        onClick={() => handleClick(index)}
+                        disabled={game.winner !== null}
+                    >
+                            {cell}
+                    </button>
+                ))}
+            </div> 
+
+            {game.winner && (
+                <button onClick={restartGame} className="restart-btn">
+                    Новая игра
                 </button>
-            ))}
+            )} 
         </div>
+        
     )
 }
 
